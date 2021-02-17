7b89f9089b531b7cef4dcf7b7b372951d9e553fa
TutoDevWeb <TutoDevWeb@outlook.fr>
2021-02-17 16:28:51 +0100

    On utilise le setState pour provoquer le ré-affichage

--- LIOR/app-lior/src/index.js
+++ LIOR/app-lior/src/index.js
@@ -10,17 +10,24 @@ class App extends React.Component {
       { id: 1, nom: "Loir Chamla" },
       { id: 2, nom: "Magali Perpin" },
       { id: 3, nom: "Jean Luois" }
-    ]
+    ],
+    compteur : 0
+  }
+  
+  handleClick = () => {
+    this.setState({compteur:this.state.compteur+1});
+    console.log(this.state);
   }
 
   render() {
     const title = "! Liste des clients !";
-    const elements = this.state.clients.map((client) => <li>{client.nom} <button>X</button></li>)
     return (
       <div>
         <h1>{title}</h1>
+        {this.state.compteur}
+        <button onClick={this.handleClick}>Click Me</button>
         <ul>
-          {elements}
+          {this.state.clients.map((client) => <li>{client.nom} <button>X</button></li>)}
         </ul>
         <form>
           <input type="text" placeholder="Ajouter un client" />
